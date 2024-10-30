8bffbeee37b66fc00b7b2426c72ea44dca24e4ca
david <s333140@studenti.polito.it>
2024-10-30 20:35:30 +0100

    prova push

---
+++ Lab1/__init__.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib as plt
+
+def exercise3():
+    list = []
+    i = int(input("inserisci un numero:"))
+    for j in range(0,i):
+        list.append[j] =int(input("inserisci elemento lista:"))
+    if (list[0] == list[i]):
+        print("elementi uguali")
